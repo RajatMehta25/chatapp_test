fbf39ffd56c4095e809b284ba3875e059705e5a4
RajatMehta <rajat.mehta@codeaegis.in>
2022-02-28 16:03:44 +0530

    Updates

---
+++ precache-manifest.035388e0bf5ac1f87913991020418922.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4289cc06c5d3d3bcf68fc106e49c07d3",
+    "url": "/chatapp_test/index.html"
+  },
+  {
+    "revision": "aeee2b92a00755ff002b",
+    "url": "/chatapp_test/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "7a708fcbd5c975e38d1b",
+    "url": "/chatapp_test/static/css/main.3e08e964.chunk.css"
+  },
+  {
+    "revision": "aeee2b92a00755ff002b",
+    "url": "/chatapp_test/static/js/2.19c7c06f.chunk.js"
+  },
+  {
+    "revision": "22c95ed68071a711843b9981e2bca1af",
+    "url": "/chatapp_test/static/js/2.19c7c06f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a708fcbd5c975e38d1b",
+    "url": "/chatapp_test/static/js/main.32bc5e5b.chunk.js"
+  },
+  {
+    "revision": "da30160a52586896c00c",
+    "url": "/chatapp_test/static/js/runtime-main.dd2575c8.js"
+  }
+]);
